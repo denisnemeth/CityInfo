98a530f0bde007588e3326e7ed0572cbe63eac8b
denisnemeth <55528169+denisnemeth@users.noreply.github.com>
2020-01-28 09:03:27 +0100

    working combo boxes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,28 +1,39 @@
 package sample;
 
+import javafx.event.Event;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Controller {
+
     public ComboBox combo_country;
     public ComboBox combo_city;
     public Label label_country;
     public Label label_city;
     public Button button_1;
     List<String> countries;
-    public Controller(){
-        Database db = new Database();
-        try{
-            db.getListOfCountries();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
+    public Controller() throws SQLException, ClassNotFoundException{
     }
-    /*public void enableButton_1(ActionEvent actionEvent){
-        while(){
-        }
-    }*/
+    public String comboValue(){
+        return (String) combo_country.getValue();
+    }
+    public void getCountries(Event event){
+        Database db = new Database();
+        combo_country.getItems().addAll(db.getCountries());
+    }
+    public void getCities(Event event){
+        Database db = new Database();
+        combo_city.getItems().addAll(db.getCities(comboValue()));
+    }
+    public void enableComboCity(ActionEvent actionEvent){
+        combo_city.setDisable(false);
+    }
+    public void enableButton_1(ActionEvent actionEvent){
+        button_1.setDisable(false);
+    }
 }
--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,27 +1,49 @@
 package sample;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
 
 public class Database {
-    private Connection connection;
-    Controller controller = new Controller();
+
     private Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://itsovy.sk:3306/world_x?serverTimezone=" + TimeZone.getDefault().getID(),
+        Connection connection = DriverManager.getConnection("jdbc:mysql://itsovy.sk:3306/world_x?serverTimezone=" + TimeZone.getDefault().getID(),
                 "student", "kosice2019");
         return connection;
     }
-    public List getListOfCountries() throws Exception {
+    public List getCountries(){
         try{
             Connection connection = getConnection();
             PreparedStatement statement = connection.prepareStatement("SELECT Name FROM country");
             ResultSet rs = statement.executeQuery();
+            String country;
+            List<String> countryList = new ArrayList<>();
             while(rs.next()){
-                controller.countries.add(rs.getString("Name"));
+                country = rs.getString("Name");
+                countryList.add(country);
             }
-            controller.combo_country.getItems().addAll(controller.countries);
+            return countryList;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List getCities(String countryName){
+        try{
+            Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT city.Name FROM city INNER JOIN country ON " +
+                    "country.Code = city.CountryCode WHERE country.Name LIKE ?");
+            statement.setString(1, countryName);
+            ResultSet rs = statement.executeQuery();
+            String city;
+            List<String> cityList = new ArrayList<>();
+            while(rs.next()){
+                city = rs.getString("Name");
+                cityList.add(city);
+            }
+            return cityList;
         }catch (Exception e){
             e.printStackTrace();
         }
